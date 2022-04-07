3420182a511949cec550a74a87945d84bec96735
DZWG <paule22@protonmail.com>
2022-04-07 20:51:23 +0200

    files missed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::{env, fs};
+use std::collections::HashSet;
 
 use env_logger;
 use log::{error, info};
+use serenity::Client;
+use serenity::framework::StandardFramework;
+use serenity::http::Http;
 use solanabeach::*;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     env_logger::init();
 
     let contents = fs::read_to_string("tokenlist.toml")
@@ -27,5 +31,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Hello, world!");
 
-    Ok(())
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let http = Http::new_with_token(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    // Create the framework
+    let framework =
+        StandardFramework::new().configure(|c| c.owners(owners).prefix("~"))
+            .group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(&token)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
 }
