74a44bf9bf73cf57437730375a830f7dd4dbb67e
DZWG <paule22@protonmail.com>
2022-07-18 21:49:00 +0200

    fix changing color if response is not correct

--- src/bot_helper.rs
+++ src/bot_helper.rs
@@ -50,55 +50,58 @@ impl EventHandler for Handler {
                         Ok(blocks) => {
                             tps = calculate_tps(blocks);
                             println!("tps: {}", tps);
-                            if tps > 0 {
-                                if tps > tps_threshold {
-                                    tps_string = format!("🚀 ~{} TPS", tps)
-                                } else {
-                                    tps_string = format!("🔥 ~{} TPS", tps)
-                                }
-                            }
                         }
                         Err(err) => {
                             error!("Error fetching solscanAPI: {:?}", err)
                         }
                     }
 
-                    //Change Nickname
-                    if !tps_string.is_empty() {
-                        for _guild in _guilds.clone() {
-                            match _guild.edit_nickname(&ctx1.http, Some(tps_string.as_str())).await {
-                                Ok(_) => { info!("Changed Bot nickname!") }
-                                Err(_) => { error!("Unable to change bot nickname!") }
-                            };
-                        }
-                    }
 
-                    //Change Bot-Color
-                    let mut red_role_id: RoleId = Default::default();
-                    let mut green_role_id: RoleId = Default::default();
-                    for guild in _guilds.clone() {
-                        let roles = guild.roles(&ctx).await;
-                        for (_role_id, role) in roles.expect("no guild roles found!") {
-                            if role.name.contains("tickers-red") {
-                                red_role_id = role.id;
-                            }
-                            if role.name.contains("tickers-green") {
-                                green_role_id = role.id;
-                            }
-                        }
-                    }
-
-
-                    for guild in &_guilds {
+                    //Only need to change anything if the respone form SOLSCAN is OK
+                    if tps > 0 {
+                        //Build String
                         if tps > tps_threshold {
-                            guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().remove_role(&ctx, red_role_id).await.unwrap();
-                            guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().add_role(&ctx, green_role_id).await.unwrap();
+                            tps_string = format!("🚀 ~{} TPS", tps)
                         } else {
-                            guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().remove_role(&ctx, green_role_id).await.unwrap();
-                            guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().add_role(&ctx, red_role_id).await.unwrap();
+                            tps_string = format!("🔥 ~{} TPS", tps)
+                        }
+
+                        //Change Nickname
+                        if !tps_string.is_empty() {
+                            for _guild in _guilds.clone() {
+                                match _guild.edit_nickname(&ctx1.http, Some(tps_string.as_str())).await {
+                                    Ok(_) => { info!("Changed Bot nickname!") }
+                                    Err(_) => { error!("Unable to change bot nickname!") }
+                                };
+                            }
+                        }
+
+                        //Get Colors
+                        let mut red_role_id: RoleId = Default::default();
+                        let mut green_role_id: RoleId = Default::default();
+                        for guild in _guilds.clone() {
+                            let roles = guild.roles(&ctx).await;
+                            for (_role_id, role) in roles.expect("no guild roles found!") {
+                                if role.name.contains("tickers-red") {
+                                    red_role_id = role.id;
+                                }
+                                if role.name.contains("tickers-green") {
+                                    green_role_id = role.id;
+                                }
+                            }
+                        }
+
+                        //Change Bot-Color
+                        for guild in &_guilds {
+                            if tps > tps_threshold {
+                                guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().remove_role(&ctx, red_role_id).await.unwrap();
+                                guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().add_role(&ctx, green_role_id).await.unwrap();
+                            } else {
+                                guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().remove_role(&ctx, green_role_id).await.unwrap();
+                                guild.member(&ctx.http, &ctx.cache.current_user_id()).await.unwrap().add_role(&ctx, red_role_id).await.unwrap();
+                            }
                         }
                     }
-
 
                     tokio::time::sleep(Duration::from_secs(env::var("LOOP_UPDATE_SLEEP").unwrap_or_else(|_| "10".to_string()).parse::<u64>().unwrap())).await;
                 }
